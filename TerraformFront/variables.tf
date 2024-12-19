dca980b35fd3cef77c629730ce22b358b62da644
dexter77 <gueye.tamsir78@gmail.com>
2024-12-19 09:53:47 +0100

    modif du fichier variable

--- TerraformFront/variables.tf
+++ TerraformFront/variables.tf
@@ -1,22 +1,27 @@
 variable "aws_access_key_id" {
   type = string
-  description = "access key of AWS"
-  default = "AWS_ACCESS_KEY_ID"
-  sensitive = true
+  # description = "access key of AWS"
+  # default = "AWS_ACCESS_KEY_ID"
+  # sensitive = true
 }
 
 variable "aws_secret_access_key" {
   type = string
-  description = "secret key of AWS"
-  default = "AWS_SECRET_ACCESS_KEY"
-  sensitive = true
+  # description = "secret key of AWS"
+  # default = "AWS_SECRET_ACCESS_KEY"
+  # sensitive = true
 }
 
+# on  variabilise la region
+variable "aws_region" {
+  type    = string
+  default = "eu-west-3"
+}
 variable "amplify_github_token" {
   type        = string
-  description = "token to connect github repo"
-  default = "AMPLIFY_GITHUB_TOKEN"
-  sensitive   = true
+  # description = "token to connect github repo"
+  # default = "AMPLIFY_GITHUB_TOKEN"
+  # sensitive   = true
 }
 
 variable "platform" {
