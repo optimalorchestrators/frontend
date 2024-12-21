3591a027e58689f8f034a571767034c6f0228ebb
dexter77 <gueye.tamsir78@gmail.com>
2024-12-21 11:36:35 +0100

    fix tests GT

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,9 +1,13 @@
 
 const { defineConfig } = require("cypress");
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/index.js',
   },
 });
